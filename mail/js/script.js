8e4e322f56f1c784bc87c7811601b30716345b1f
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-17 16:11:55 +0200

    Add for adjustment

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,21 +4,21 @@
 
 let mail = prompt("inserisci la tua mail");
 
-const gmail = 'pincopanco95@gmail.com';
+const Jess = 'Jessica_fletcher@cabotcove.com';
 const shield = 'blackwidow@avengers.it';
 const alice_mail = 'charlies_angels@alice.it';
 const hogwarts = 'harry.potter@diagonAlley.it';
 const hobbit_town = 'frodo.baggins@lor.com';
 const Mulan = 'Hua_Mulan@disney.cn';
 
-const listMail = [gmail, shield, alice_mail, hogwarts, hobbit_town, Mulan];
+const listMail = [Jess, shield, alice_mail, hogwarts, hobbit_town, Mulan];
 console.log(listMail)
 
 const test = 'true'
 const test1 = 'false'
 
-for(i = listMail; i < listMail.lenght;  i++) {
-    if (listMail[i] == test) {
+for(let i=0; i<listMail.lenght; i++) {
+    if (listMail == test) {
         alert('Il tuo indirizzo mail è registrato!')
     } else if (listMail[i] == test1) {
         alert('Siamo spiacenti, la tua mail non è valida o non è registrata!')
